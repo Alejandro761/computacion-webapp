6075ed04689b3750779aba54a91e46b550b517f4
Alejandro Castaneda <alejandroxd62@gmail.com>
2024-02-08 20:16:22 -0600

    realice la estructura del navbar, asi como el codigo ts

--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  @Input() tipo: string = ''; //asi se define una variable de tipo input (de tipo decorador) para la entrada de datos
+
+  public token: string = '';
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  public logout() {
+
+  }
 }
